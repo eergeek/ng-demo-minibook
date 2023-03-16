2889e6ac126f35c498a7e3890173c07ede1eb1cc
jambu <kadamjd99@gmail.com>
2023-03-16 17:59:33 +0530

    added search component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'search',
+    component: SearchComponent
+  },
+  {
+    path: '',
+    redirectTo: '/search',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes), FormsModule],
+  exports: [RouterModule, FormsModule]
 })
 export class AppRoutingModule { }
